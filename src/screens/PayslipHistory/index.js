aacd63e06d788540bf55c016b9c14a696075da37
MAC <withyouamalive@yahoo.com>
2022-07-26 11:47:14 +0100

    fixing bug

--- src/screens/PayslipHistory/index.js
+++ src/screens/PayslipHistory/index.js
@@ -49,45 +49,45 @@ export default function PayslipHistory({tab_name}) {
           </Text>
       </View>
       <View style={styles.line} />
-      <View style={styles.container}>
-            {
-              years && Array.isArray(years) ? <ScrollView horizontal>
-                  {
-                    years.map((item,i)=><TouchableOpacity  onPress={() => setYear(item)}
-                      key={i}
-                    >
-                      <Container width={15} padding={2} marginRight={1}>
-                        {
-                          year === item ?  <H1 textAlign="center">{item}</H1> :  <P textAlign="center">{item}</P>
-                        }
-                      </Container>
-                    </TouchableOpacity>)
-                  }
-              </ScrollView> : null
-            }
 
-            <Container marginTop={3} width={90}>
-              <FlatList
-                data={history?.data && Array.isArray(history?.data) ? history?.data : []}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={renderItem}
-                removeClippedSubviews={true}
-                showsVerticalScrollIndicator={false}
-                ListEmptyComponent={()=>{
-                  return tab_name === 'history' ? <EmptyStateWrapper 
-                  icon={Images.EmptyPayslip}
-                  header_1={"You have no"}
-                  header_2={"payroll history"}
-                  sub_text={"They will show up here when you do."}
-                /> :  <EmptyStateWrapper 
-                icon={Images.EmptyPayslip}
-                header_1={"You have no"}
-                header_2={"generated payslip"}
-                sub_text={"They will show up here when you do."}
-              />}
+      <Container width={90} alignSelf="center" marginTop={3}>
+          {
+            years && Array.isArray(years) ? <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                {
+                  years.map((item,i)=><TouchableOpacity  onPress={() => setYear(item)}
+                    key={i}
+                  >
+                    <Container width={15} padding={2} marginRight={1}>
+                      {
+                        year === item ?  <H1 textAlign="center">{item}</H1> :  <P textAlign="center">{item}</P>
+                      }
+                    </Container>
+                  </TouchableOpacity>)
                 }
-              />
-            </Container>
+            </ScrollView> : null
+          }
+        </Container>
+      <View style={styles.container}>
+          <FlatList
+            data={history?.data && Array.isArray(history?.data) ? history?.data : []}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={renderItem}
+            removeClippedSubviews={true}
+            showsVerticalScrollIndicator={false}
+            ListEmptyComponent={()=>{
+              return tab_name === 'history' ? <EmptyStateWrapper 
+              icon={Images.EmptyPayslip}
+              header_1={"You have no"}
+              header_2={"payroll history"}
+              sub_text={"They will show up here when you do."}
+            /> :  <EmptyStateWrapper 
+            icon={Images.EmptyPayslip}
+            header_1={"You have no"}
+            header_2={"generated payslip"}
+            sub_text={"They will show up here when you do."}
+          />}
+            }
+          />
       </View>
     </ScreenWrapper>
   );
--- src/screens/PayslipHistory/styles.js
+++ src/screens/PayslipHistory/styles.js
@@ -5,7 +5,7 @@ import { FontFamily } from '../../utills/FontFamily';
 
 const styles = StyleSheet.create({
   container: {
-    //flex: 1,
+    flex: 1,
     marginVertical: height(3),
     alignItems : "center",
   },
