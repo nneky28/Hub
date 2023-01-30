aa586525d9cb537150fc8483c76f81fa88b58adc
Ezema Nneka <ezemanneka@28@gmail.com>
2023-01-30 13:45:01 +0100

    conflict resolved

--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Routes from './Routes/index';
 import 'react-native-gesture-handler';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './Redux/index';
 import FlashMessage from 'react-native-flash-message';
-import {LogBox} from 'react-native';
+import { LogBox } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 LogBox.ignoreAllLogs(true);
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primaryButton: '#2898A4',
+    whiteColor: '#ffffff',
+    darkColor: '#3E3E3E',
+    fadeDarkColor: '#717171',
+    fadeDarkColor2: '#8F8F8F',
+    // primaryButton: 'green',
+    secondaryButon: 'yellow',
+    primaryText: '#000',
+    secondaryText: 'red'
+  }
+}
 export default function App() {
   useEffect(() => {
     LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
   }, []);
   return (
     <Provider store={store}>
-      <Routes />
-      <FlashMessage position="bottom" icon="auto" />
+      <PaperProvider theme={theme}>
+        <Routes />
+        <FlashMessage position="bottom" icon="auto" />
+      </PaperProvider>
     </Provider>
   );
 }
