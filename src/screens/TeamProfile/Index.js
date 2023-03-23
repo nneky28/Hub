4d5673ae3d24d18a58244edf486249c0f4ddb037
Ezema Nneka <ezemanneka@28@gmail.com>
2023-03-23 16:29:46 +0100

    well achieved

--- src/screens/TeamProfile/Index.js
+++ src/screens/TeamProfile/Index.js
@@ -120,7 +120,7 @@ const Index = ({ route }) => {
             <View style={styles.emptyState}>
                 <View>
                     <P style={styles.emptyText}>{departments ? (`${item?.name} department`) : item?.first_name}</P>
-                    <P style={styles.emptyText}> has no {actionTitle === "To-Do" && tab === 'All' ? 'To do.' : actionTitle === "To-Do" && tab ? (`task ${tab}`) : (actionTitle === "In Progress" ? "task In Progress." : `${actionTitle} Task.`)}</P>
+                    <P style={styles.emptyText}> has no {actionTitle === "To-Do" && tab === 'All' ? ' task To-Do.' : actionTitle === "To-Do" && tab ? (`task ${tab}`) : (actionTitle === "In Progress" ? "task In Progress." : `${actionTitle} Task.`)}</P>
                 </View>
             </View>
         );
