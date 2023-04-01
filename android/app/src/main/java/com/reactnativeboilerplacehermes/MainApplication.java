2e4cfc3acbcb5695c14f3e6163883d8ff2e33737
Admin <withyouamalive@yahoo.com>
2023-04-01 15:40:28 +0100

    push notification added

--- android/app/src/main/java/com/reactnativeboilerplacehermes/MainApplication.java
+++ android/app/src/main/java/com/reactnativeboilerplacehermes/MainApplication.java
@@ -9,15 +9,14 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JSIModulePackage; // <- add
 import com.facebook.soloader.SoLoader;
-import com.imagepicker.ImagePickerPackage;
+//import com.imagepicker.ImagePickerPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import androidx.multidex.MultiDexApplication;
 
-public class MainApplication extends MultiDexApplication implements ReactApplication {
+public class MainApplication extends Application implements ReactApplication {
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
 
     @Override
