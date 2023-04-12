4952d838cbac6d3c86b11b2db4cc62126683780a
Admin <withyouamalive@yahoo.com>
2023-04-12 09:58:09 +0100

    made changes to benefits

--- src/screens/Benefits/index.tsx
+++ src/screens/Benefits/index.tsx
@@ -1,59 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View} from 'react-native';
 import BenifitList from '../../components/BenifitList';
+import { HomePageHeader } from '../../components/Headers/CustomHeader';
 import ScreenWrapper from '../../components/ScreenWrapper';
-import { useFetchAboutMe, useFetchBenefits } from '../../utills/api';
-import { BackHandler, Container, CustomWebView, H1, PageLoader } from '../../utills/components';
+import {useFetchAboutMeProps} from '../../components/TimeoffModal/types';
+import {useFetchAboutMe, useFetchBenefits} from '../../utills/api';
+import {Images} from "../../utills/Image"
+import {
+  Container,
+  CustomWebView,
+  H1,
+  PageLoader,
+} from '../../utills/components';
 import styles from './styles';
-import { useFetchAboutMeProps } from '../../components/TimeoffModal/types';
-
 
 
 export default function Benefits() {
-  const [web, setWeb] = useState(false);
+  const [web, setWeb] = useState<boolean>(false);
   const [web_url, setWebUrl] = useState<string | null>(null);
 
+  const {data: profile} = useFetchAboutMe('main') as useFetchAboutMeProps;
 
-  const {
-    data : about,
-  } = useFetchAboutMe("main") as useFetchAboutMeProps
+  const {data: benefits = {results: []}, isLoading} = useFetchBenefits(
+    profile?.id,
+  );
 
-  const {
-    data: benefits,
-    isLoading,
-  } = useFetchBenefits(about?.id);
-
-
-
-  const goToWeb = (url: string) => {
+  const goToWeb = (url: string | null) => {
     setWebUrl(url);
     setWeb(true);
   };
   const closeWeb = () => {
     setWeb(false);
   };
-
-  useEffect(() => {
-  }, [about]);
+  React.useEffect(() => {}, [profile]);
 
   return (
-    <ScreenWrapper scrollEnabled={false}>
-      {web && web_url ? (
-        <CustomWebView setShow={closeWeb} web_url={web_url} />
-      ) : (
-        <>
-          <View style={styles.header}>
-            <BackHandler />
-            <Text numberOfLines={1} style={styles.screenTitle}>
-              Benefits
-            </Text>
-          </View>
-          <View style={styles.line} />
-
+    <ScreenWrapper>
+      <React.Fragment>
+          <HomePageHeader header='Benefits' 
+            image={Images.people}
+          />
           <View style={styles.mainViewContainer}>
             {isLoading ? <PageLoader /> : null}
             {!isLoading ? (
-              <>
+              <React.Fragment>
                 <Container width={90} marginTop={2}>
                   <H1 fontSize={3.4}>Company benefits you are enrolled on</H1>
                 </Container>
@@ -61,19 +51,19 @@ export default function Benefits() {
                   data={['#C2D4FF', '#99E6FF']}
                   horizontal={false}
                   benefits={
-                    benefits?.results &&
-                    Array.isArray(benefits?.results) &&
-                    benefits?.results.length > 0
-                      ? benefits?.results
+                    Array.isArray((benefits as {results: Array<any>}).results)
+                      ? (benefits as {results: Array<any>}).results
                       : []
                   }
                   goToWeb={goToWeb}
                 />
-              </>
+              </React.Fragment>
             ) : null}
           </View>
-        </>
-      )}
+        {web && web_url ? 
+          <CustomWebView setShow={closeWeb} web_url={web_url} />
+        : null}
+      </React.Fragment>
     </ScreenWrapper>
   );
 }
